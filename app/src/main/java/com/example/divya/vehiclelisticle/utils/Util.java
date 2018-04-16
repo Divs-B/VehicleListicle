686c4686ad2d845ce6f6d831547f4f0f11a472c4
Divs-B <nikita16>
2018-04-16 02:03:51 +0100

    util

---
+++ app/src/main/java/com/example/divya/vehiclelisticle/utils/Util.java
@@ -0,0 +1,14 @@
+package com.example.divya.vehiclelisticle.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class Util {
+    public static boolean isConnected(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        return info == null ? false : info.isConnected();
+    }
+}
